c8f23f265c0bca49eec0cd823f05fbee7060fc1d
Elysia-ff <jhk19960@gmail.com>
2023-07-22 20:06:35 +0900

    moving window

--- Assets/Scripts/Foundations/UserInterfaces/UIManager.cs
+++ Assets/Scripts/Foundations/UserInterfaces/UIManager.cs
@@ -15,18 +15,26 @@ namespace Elysia.UI
         public UIBase FocusedUI { get; private set; }
 
         private RectTransform _windowContainer;
+        private UIBase _movingUI;
+        private Vector2 _movingUIDelta;
+        private int _uiMoveLayer;
 
         private readonly Dictionary<Type, UIBase> _uiPrefabs = new Dictionary<Type, UIBase>();
         private readonly Dictionary<Type, IObjectPool<UIBase>> _cachedUIs = new Dictionary<Type, IObjectPool<UIBase>>();
 
         private readonly List<RaycastResult> _raycastResults = new List<RaycastResult>();
 
+        private const float SCREEN_MARGIN = 20f;
+
         public UIManager Initialize()
         {
             Camera = transform.Find("Camera").GetComponent<Camera>();
             _windowContainer = (RectTransform)transform.Find("WindowCanvas/Container");
+            _uiMoveLayer = LayerMask.NameToLayer("UI Move");
 
             Game.Scene.Input.BindMouse(0, EInputType.Pressed, OnLMBPressed);
+            Game.Scene.Input.BindMouse(0, EInputType.Holding, OnLMBHolding);
+            Game.Scene.Input.BindMouse(0, EInputType.Released, OnLMBReleased);
 
             return this;
         }
@@ -62,6 +70,8 @@ namespace Elysia.UI
             {
                 return;
             }
+
+            _movingUI = null;
 
             FocusedUI = ui;
 
@@ -101,7 +111,43 @@ namespace Elysia.UI
                 RaycastResult result = _raycastResults[0];
                 UIBase ui = result.gameObject.GetComponentInParent<UIBase>();
                 FocusUI(ui);
+
+                if (result.gameObject.layer == _uiMoveLayer)
+                {
+                    _movingUI = ui;
+                    Vector2 uiPosition = ScreenPointToUIPosition(position);
+                    _movingUIDelta = uiPosition - ui.RectTransform.localPosition.XY();
+                }
             }
+        }
+
+        private void OnLMBHolding(Vector2 position, EModifier modifier)
+        {
+            if (!_movingUI)
+            {
+                return;
+            }
+
+            position.x = Mathf.Clamp(position.x, SCREEN_MARGIN, Screen.width - SCREEN_MARGIN);
+            position.y = Mathf.Clamp(position.y, SCREEN_MARGIN, Screen.height - SCREEN_MARGIN);
+
+            Vector2 uiPosition = ScreenPointToUIPosition(position);
+            _movingUI.RectTransform.localPosition = uiPosition - _movingUIDelta;
+        }
+
+        private void OnLMBReleased(Vector2 position, EModifier modifier)
+        {
+            _movingUI = null;
+        }
+
+        public Vector2 ScreenPointToUIPosition( Vector2 screenPosition)
+        {
+            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(_windowContainer, screenPosition, Camera, out Vector2 localPosition))
+            {
+                return localPosition;
+            }
+
+            return Vector2.zero;
         }
     }
 }
